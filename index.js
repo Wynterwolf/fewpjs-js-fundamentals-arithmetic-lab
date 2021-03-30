24129b372bd7391ceeb15085a69d00c4154c17a8
Samantha Jordan <shinyguns@gmail.com>
2021-03-30 18:58:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 1
+let num2 = 62
+
+let num3=13
+let num4 = 9
+
+// multiply()
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = Math.floor(num3 % num4)
+
+const max = Math.max(1,2,5,20)
